beb6f354cc7ace4a33c34ba65336011b3eb8f32a
Josh Hudson <jhudson@chef.io>
2017-06-21 14:28:39 -0600

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# backend_search_cluster
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: backend_search_cluster
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
