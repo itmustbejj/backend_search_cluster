d287437d96fad9a629df9efe6fb8027e029387d1
echohack <echohack@users.noreply.github.com>
2017-07-05 16:55:48 -0700

    fix typo
    Signed-off-by: echohack <echohack@users.noreply.github.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,6 +3,6 @@ default['java']['install_flavor'] = 'oracle'
 default['java']['oracle']['accept_oracle_download_terms'] = true
 
 default['logstash_heap_size'] = '2g'
-default['logstash_workers'] = (node['cpu']['cores'].to_i *.75).floor  # logstash workers at a 3:4 ratio to cores
+default['logstash_workers'] = (node['cpu']['cores'].to_i * 0.75).floor  # logstash workers at a 3:4 ratio to cores
 default['logstash_bulk_size'] = '512'
 default['logstash_extra_procs'] = '3'
