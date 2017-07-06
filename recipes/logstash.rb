d47df7f013eb96a19a4d3eaa409feb3cb5241edd
Josh Hudson <jhudson@chef.io>
2017-07-06 11:49:40 -0600

    Rename logstash_total_procs attribute to reflect loop logic
    Signed-off-by: Josh Hudson <jhudson@chef.io>

--- recipes/logstash.rb
+++ recipes/logstash.rb
@@ -14,7 +14,7 @@ end
 
 execute 'create multiple logstash processes' do
   command <<-EOH
-    for ii in $(seq 2 #{node['logstash_extra_procs']}); do
+    for ii in $(seq 2 #{node['logstash_total_procs']}); do
       cp -r /opt/delivery/sv/logstash/ /opt/delivery/sv/logstash$ii
         cp -r /opt/delivery/embedded/etc/logstash/conf.d /opt/delivery/embedded/etc/logstash/conf.d$ii
         rm /opt/delivery/embedded/etc/logstash/conf.d$ii/10-websocket-output.conf
