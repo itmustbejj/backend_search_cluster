2e30f52d12f86c292a1f5877e561045acf78116c
Josh Hudson <jhudson@chef.io>
2017-07-10 18:25:13 -0600

    Change attribute namespace for logstash tunables
    Signed-off-by: Josh Hudson <jhudson@chef.io>

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@ default['java']['jdk_version'] = '8'
 default['java']['install_flavor'] = 'oracle'
 default['java']['oracle']['accept_oracle_download_terms'] = true
 
-default['logstash_heap_size'] = '2g'
-default['logstash_workers'] = (node['cpu']['cores'].to_i * 0.75).floor  # logstash workers at a 3:4 ratio to cores
-default['logstash_bulk_size'] = '512'
-default['logstash_total_procs'] = '4'
+default['logstash']['heap_size'] = '2g'
+default['logstash']['workers'] = (node['cpu']['cores'].to_i * 0.75).floor  # logstash workers at a 3:4 ratio to cores
+default['logstash']['bulk_size'] = '512'
+default['logstash']['total_procs'] = '4'
--- recipes/logstash.rb
+++ recipes/logstash.rb
@@ -6,15 +6,15 @@
 
 execute 'tune logstash' do
   command <<-EOH
-  sed -i s/LS_HEAP_SIZE=[0-9]*m/LS_HEAP_SIZE=#{node['logstash_heap_size']}/ /opt/delivery/sv/logstash/run
-  sed -i s/-w\\ [0-9]*/-w\\ #{node['logstash_workers']}/ /opt/delivery/sv/logstash/run
-  sed -i s/-b\\ [0-9]*/-b\\ #{node['logstash_bulk_size']}/ /opt/delivery/sv/logstash/run
+  sed -i s/LS_HEAP_SIZE=[0-9]*m/LS_HEAP_SIZE=#{node['logstash']['heap_size']}/ /opt/delivery/sv/logstash/run
+  sed -i s/-w\\ [0-9]*/-w\\ #{node['logstash']['workers']}/ /opt/delivery/sv/logstash/run
+  sed -i s/-b\\ [0-9]*/-b\\ #{node['logstash']['bulk_size']}/ /opt/delivery/sv/logstash/run
   EOH
 end
 
 execute 'create multiple logstash processes' do
   command <<-EOH
-    for ii in $(seq 2 #{node['logstash_total_procs']}); do
+    for ii in $(seq 2 #{node['logstash']['total_procs']}); do
       cp -r /opt/delivery/sv/logstash/ /opt/delivery/sv/logstash$ii
         cp -r /opt/delivery/embedded/etc/logstash/conf.d /opt/delivery/embedded/etc/logstash/conf.d$ii
         rm /opt/delivery/embedded/etc/logstash/conf.d$ii/10-websocket-output.conf
