8700c8fba613ebedb19ca38c59a1422496297f3f
Josh Hudson <jhudson@chef.io>
2017-07-12 13:01:29 -0600

    Use cpu['total'] instead of cpu['cores'] when calculating logstash workers
    Signed-off-by: Josh Hudson <jhudson@chef.io>

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@ default['java']['install_flavor'] = 'oracle'
 default['java']['oracle']['accept_oracle_download_terms'] = true
 
 default['logstash']['heap_size'] = '2g'
-default['logstash']['workers'] = (node['cpu']['cores'].to_i * 0.75).floor  # logstash workers at a 3:4 ratio to cores
+default['logstash']['workers'] = (node['cpu']['total'].to_i * 0.75).floor  # logstash workers at a 3:4 ratio to cores
 default['logstash']['bulk_size'] = '512'
 default['logstash']['total_procs'] = '4'
 
