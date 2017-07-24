04c7edacd4390fd596b0ec36df6d2cf344debd4c
Josh Hudson <jhudson@chef.io>
2017-07-24 17:46:38 -0600

    Porting logic from chef-apply script
    Signed-off-by: Josh Hudson <jhudson@chef.io>

--- recipes/logstash.rb
+++ recipes/logstash.rb
@@ -1,39 +1,25 @@
-#
-# Cookbook:: backend_search_cluster
-# Recipe:: logstash
-#
-# Copyright:: 2017, The Authors, All Rights Reserved.
-
 ruby_block 'configure logstash settings in delivery.rb' do
   block do
     delivery_rb = Chef::Util::FileEdit.new('/etc/delivery/delivery.rb')
     unless delivery_rb.search_file_replace_line(
-      /delivery['logstash']['config']['pipeline']['batch']['size']/,
-      "delivery['logstash']['config']['pipeline']['batch']['size'] = #{node['logstash']['bulk_size']}"
+      /logstash\['config'\]/,
+      "logstash['config'] = { 'pipeline' => { 'batch' => { 'size' => #{node['logstash']['bulk_size']} }, 'workers' => #{node['logstash']['workers']} } }"
     )
       delivery_rb.insert_line_if_no_match(
-        /delivery['logstash']['config']['pipeline']['batch']['size']/,
-        "delivery['logstash']['config']['pipeline']['batch']['size'] = #{node['logstash']['bulk_size']}"
+        /logstash\['config'\]/,
+      "logstash['config'] = { 'pipeline' => { 'batch' => { 'size' => #{node['logstash']['bulk_size']} }, 'workers' => #{node['logstash']['workers']} } }"
       )
     end
+    delivery_rb.write_file
 
+    delivery_rb = Chef::Util::FileEdit.new('/etc/delivery/delivery.rb')
     unless delivery_rb.search_file_replace_line(
-      /delivery['logstash']['heap_size']/,
-      "delivery['logstash']['heap_size'] = #{node['logstash']['heap_size']}"
+      /logstash\['heap_size'\]/,
+      "logstash['heap_size'] = '#{node['logstash']['heap_size']}'"
     )
       delivery_rb.insert_line_if_no_match(
-        /delivery['logstash']['heap_size']/,
-        "delivery['logstash']['heap_size'] = #{node['logstash']['heap_size']}"
-      )
-    end
-
-    unless delivery_rb.search_file_replace_line(
-      /delivery['logstash']['pipeline']['workers']/,
-        "delivery['logstash']['pipeline']['workers'] = #{node['logstash']['workers']}"
-    )
-      delivery_rb.insert_line_if_no_match(
-        /delivery['logstash']['pipeline']['workers']/,
-        "delivery['logstash']['pipeline']['workers'] = #{node['logstash']['workers']}"
+        /logstash\['heap_size'\]/,
+      "logstash['heap_size'] = '#{node['logstash']['heap_size']}'"
       )
     end
     delivery_rb.write_file
@@ -45,8 +31,8 @@ end
 ruby_block 'reap extra logstash workers' do
   block do
     logstash_dirs = Dir['/opt/delivery/sv/logstash*']
-    if node['logstash']['total_procs'].to_i < logstash_dirs.length
-      (node['logstash']['total_procs'].to_i + 1..logstash_dirs.length).each do |i|
+    if node['logstash']['total_procs'] < logstash_dirs.length
+      (node['logstash']['total_procs'] + 1..logstash_dirs.length).each do |i|
         system("sudo automate-ctl stop logstash#{i}")
         FileUtils.rm_rf("/opt/delivery/sv/logstash#{i}")
         FileUtils.rm_rf("/opt/delivery/embedded/etc/logstash/conf.d#{i}")
@@ -55,7 +41,7 @@ ruby_block 'reap extra logstash workers' do
   end
 end
 
-(2..node['logstash']['total_procs'].to_i).each do |i|
+(2..node['logstash']['total_procs']).each do |i|
   unless Dir.exist?("/opt/delivery/sv/logstash#{i}")
     execute "copy logstash sv dir for logstash#{i}" do
       command "cp -r /opt/delivery/sv/logstash/ /opt/delivery/sv/logstash#{i}"
@@ -83,9 +69,9 @@ end
     block do
       ls_run_file = Chef::Util::FileEdit.new("/opt/delivery/sv/logstash#{i}/run")
       ls_run_file.search_file_replace(/conf\.d/, "conf.d#{i}")
-      ls_run_file.search_file_replace(/-w [0-9]+/, "-w #{node['logstash']['workers']}")
-      ls_run_file.search_file_replace(/-b [0-9]+/, "-w #{node['logstash']['bulk_size']}")
-      ls_run_file.search_file_replace(/LS_HEAP_SIZE=[0-9]*m/, "LS_HEAP_SIZE=#{node['logstash']['heap_size']}")
+      ls_run_file.search_file_replace(/-w (?<!#{node['logstash']['workers']})[0-9]+/, "-w #{node['logstash']['workers']}")
+      ls_run_file.search_file_replace(/-b (?<!#{node['logstash']['bulk_size']})[0-9]+/, "-b #{node['logstash']['bulk_size']}")
+      ls_run_file.search_file_replace(/LS_HEAP_SIZE=(?<!#{node['logstash']['heap_size']})[0-9]*m/, "LS_HEAP_SIZE=#{node['logstash']['heap_size']}")
       ls_run_file.write_file
       ls_log_run_file = Chef::Util::FileEdit.new("/opt/delivery/sv/logstash#{i}/log/run")
       ls_log_run_file.search_file_replace(/logstash/, "logstash#{i}")
